ea9b13f97485a0753ccd2a3f20945d287e339ecd
Sourabh bhatt <bhattsourabh20@gmail.com>
2021-04-27 15:59:01 +0530

    first

---
+++ app/src/main/java/com/application/todoapp/data/Converter.kt
@@ -0,0 +1,18 @@
+package com.application.todoapp.data
+
+import androidx.room.TypeConverter
+import com.application.todoapp.data.models.Priority
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+
+}
---
+++ app/src/main/java/com/application/todoapp/data/models/Priority.kt
@@ -0,0 +1,7 @@
+package com.application.todoapp.data.models
+
+enum class Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
